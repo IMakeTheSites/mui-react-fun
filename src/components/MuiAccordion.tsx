07d6427123825f2179a918f01706a2c3770c29fa
Mark Wlodawski <imakethesites@gmail.com>
2022-11-20 19:56:28 -0500

    Add MUI Accordion

---
+++ src/components/MuiAccordion.tsx
@@ -0,0 +1,80 @@
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useState } from "react";
+
+export const MuiAccordion = () => {
+  const [expanded, setExpanded] = useState<string | false>(false);
+  const handleChange = (isExpanded: boolean, panel: string) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div>
+      <Accordion
+        expanded={expanded === "panel1"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel1")}
+      >
+        <AccordionSummary
+          id="panel1-header"
+          aria-controls="panel1-content"
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <Typography>Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+            culpa nostrum eaque rem illo perspiciatis minima quae obcaecati odio
+            nihil. Amet quaerat perspiciatis ad fuga delectus ipsa voluptatibus
+            iure in.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel2"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel2")}
+      >
+        <AccordionSummary
+          id="panel2-header"
+          aria-controls="panel2-content"
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <Typography>Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+            culpa nostrum eaque rem illo perspiciatis minima quae obcaecati odio
+            nihil. Amet quaerat perspiciatis ad fuga delectus ipsa voluptatibus
+            iure in.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        expanded={expanded === "panel3"}
+        onChange={(event, isExpanded) => handleChange(isExpanded, "panel3")}
+      >
+        <AccordionSummary
+          id="panel3-header"
+          aria-controls="panel3-content"
+          expandIcon={<ExpandMoreIcon />}
+        >
+          <Typography>Accordion 3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi
+            culpa nostrum eaque rem illo perspiciatis minima quae obcaecati odio
+            nihil. Amet quaerat perspiciatis ad fuga delectus ipsa voluptatibus
+            iure in.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+};
