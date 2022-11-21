3658161c9235fbbf10b744a9827e08ba652fce4d
Mark Wlodawski <imakethesites@gmail.com>
2022-11-20 21:26:19 -0500

    Add Mui Navbar Component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { MuiCard } from "./components/MuiCard";
 import { MuiCheckbox } from "./components/MuiCheckbox";
 import { MuiImageList } from "./components/MuiImageList";
 import { MuiLayout } from "./components/MuiLayout";
+import { MuiNavbar } from "./components/MuiNavbar";
 import { MuiRadioButton } from "./components/MuiRadioButton";
 import { MuiRating } from "./components/MuiRating";
 import { MuiSelect } from "./components/MuiSelect";
@@ -28,7 +29,8 @@ function App() {
       {/* <MuiLayout /> */}
       {/* <MuiCard /> */}
       {/* <MuiAccordion /> */}
-      <MuiImageList />
+      {/* <MuiImageList /> */}
+      <MuiNavbar />
     </div>
   );
 }
